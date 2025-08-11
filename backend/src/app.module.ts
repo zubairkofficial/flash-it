a676bbc4c0d0a65cab432fca31560fbece6b4739
EngrMuqeetAhmad <hahmadqayyum@gmail.com>
2025-08-11 05:06:54 -0700

    initialized frontend and backend

---
+++ backend/src/app.module.ts
@@ -0,0 +1,36 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { JwtModule } from '@nestjs/jwt';
+
+@Module({
+  imports: [
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: process.env.JWT_TOKEN_EXPIRATION },
+    }),
+    SequelizeModule.forRoot({
+      dialect: process.env.DB_DIALECT as any,
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadModels: true,
+      models: [],
+      synchronize: process.env.DB_SYNCHRONIZE == 'true' ? true : false,
+      logging: true,
+
+      sync: {
+        force: process.env.NODE_ENV == 'development' ? false : false,
+        alter: process.env.NODE_ENV == 'development' ? true : false,
+      },
+      retryDelay: 3000,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
