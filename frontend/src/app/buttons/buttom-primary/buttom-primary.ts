73ed77f96b00e8759fff7c989e575e3d0b827b1a
EngrMuqeetAhmad <hahmadqayyum@gmail.com>
2025-08-12 07:23:04 -0700

    initialized building signup page

--- frontend/src/app/buttons/buttom-primary/buttom-primary.ts
+++ frontend/src/app/buttons/buttom-primary/buttom-primary.ts
@@ -1,19 +1,15 @@
-import { CommonModule } from '@angular/common';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-buttom-primary',
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './buttom-primary.html',
   styleUrl: './buttom-primary.css',
 })
 export class ButtomPrimary {
   @Input() text: string = 'Primary';
   @Input() styles: string = '';
-  // concatClass = '';
-  // ngOnInit(): void {
-  //   // this.concatClass = 'btn btn-primary' + ' ' + this.styles;
-  // }
+  
 
   get concatClass(): string[] {
     return ['btn', 'btn-primary', ...this.styles.trim().split(/\s+/)];
---
+++ frontend/src/app/sign-up/sign-up-layout/sign-up-layout.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-up-layout',
+  imports: [],
+  templateUrl: './sign-up-layout.html',
+  styleUrl: './sign-up-layout.css'
+})
+export class SignUpLayout {
+
+}
