18052c0969513310f1c2044791df38e3919a9a41
EngrMuqeetAhmad <hahmadqayyum@gmail.com>
2025-09-29 05:18:33 -0700

    fix: DEV and PROD urls

--- frontend/src/utils/global.utils.ts
+++ frontend/src/utils/global.utils.ts
@@ -3,9 +3,9 @@ import { Injectable } from '@angular/core';
 class globals {
   private NODE_ENV: string = 'development';
 
-  // private DEV_URL: string = 'http://localhost:3000';
-  private DEV_URL: string = 'http://13.60.198.16/backend';
-  private PROD_URL: string = 'https://api.example.com';
+  private DEV_URL: string = 'http://localhost:3000';
+  
+  private PROD_URL: string = 'http://13.60.198.16/backend';
   private URL: string =
     this.NODE_ENV === 'development' ? this.DEV_URL : this.PROD_URL;
 
